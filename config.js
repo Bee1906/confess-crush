efdf3699c3eb63baee76bdcea6e6e9f84a6988c2
Bee1906 <tntt1906@gmail.com>
2024-09-29 10:30:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,24 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    titleWeb: "PhucHoanggisMine",
+    introTitle: 'Phúc Hoàngg',
+    introDesc: ` Đoán xem em  đang làm gì?
+    Vì hôm nay là Cn nên em rảnh vailonnnn
+    Em ngồi đây làm những thứ linh tinh này
+    Gửi anhh yêu của em nhó
+    Em yêu anhhhh lắm lắm lắm lắm luôn ó `,
+    btnIntro: 'ehhehehehe',
+    title: 'Em rất xinh đúng khônggg 🥰',
+    desc: ' Anh có vuii khi ở bên cạnh em khumm ?
+        Phải chăng anh đã say mê ngay từ giây phút đầu tiên rồi đúng khumm
+        ', Anh mê em lắmmmmm chứ gì 
+    btnYes: ' Mê em vãiiii <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
+    question: ' Trongg lòng em chỉ có Phúc thoaii , nên anh đừng có lo <3',
+    btnReply: ' Phúc cũng vậyy nhó <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    mess: 'Em biết mà 🥰. Yêu anhhh nhiều nhiều 😘😘',
+    messDesc: 'Cơ mà em giận anh cmnr , Sao anh mê ngủ bỏ rơi em hoài dãyyyy 
+            Ngủ nó ngon hơn em hả ????? .',
+    btnAccept: 'Anh xin lỗi , anh là con heoo , anh mê ăn mê ngủ , anhh xin lỗiiiii , anhh đền pù cho em nhé <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
